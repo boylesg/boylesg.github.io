099861358e4e12dcf59c2e171012c9704c0a4fe2
Gregary Boyles <gregplants@bigpond.com>
2023-08-07 03:05:25 +1000

    Add files via upload

---
+++ GregsSTEMTuition/Common.js
@@ -0,0 +1,125 @@
+﻿//**********************************************************************************************************************
+//**********************************************************************************************************************
+//** GLOBAL VARIBALES
+//**********************************************************************************************************************
+//**********************************************************************************************************************
+
+var g_arrayTOC = [];
+var g_bCoursesPopupMenu = false;
+
+
+
+
+//**********************************************************************************************************************
+//**********************************************************************************************************************
+//** COMMON FUNCTIONS
+//**********************************************************************************************************************
+//**********************************************************************************************************************
+
+function GenerateTryItNow()
+{
+	var divTryItNowHTML = document.getElementById("TryItNowHTML"),
+		strHTML = "";
+	
+	if (divTryItNowHTML)
+	{
+		strHTML = strHTML.innerHTML;
+		document.write(strHTML);
+	}
+}
+
+function GenerateGregsEmailAddress()
+{
+	var strEmailAddress = "gregplants" + "@" + "bigpond" + "." + "com";
+	document.write("<a class=\"Email\" id=\"Email\" href=\"mailto: " + strEmailAddress + "\">" + strEmailAddress + "</a>");
+}
+
+function GenerateGregsMobile()
+{
+	var strMobile = "04" + "55" + "328" + "886";
+	document.write(strMobile);
+}
+
+function SpaceGap(nNumberSpaces)
+{
+	for (var nI = 0; nI < nNumberSpaces; nI++)
+		document.write("&nbsp;");
+}
+
+var g_nLevel = 0;
+						
+function BuildTOC(arrayTOC, g_nLevel)
+{
+	var strIndent = "", nI = 0;
+	
+	for (nI = 0; nI < g_nLevel; nI++)
+		strIndent += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+		
+	for (nI = 0; nI < arrayTOC.length; nI++)
+	{
+		if (Array.isArray(arrayTOC[nI]))
+		{
+			BuildTOC(arrayTOC[nI], g_nLevel + 1);
+		}
+		else
+		{
+			document.write("<li class=\"TOCListItem\">&nbsp;&nbsp;" + strIndent + "<a class=\"TOCLink\" href=\"#" + arrayTOC[nI] + "\">" + arrayTOC[nI] + "</a></li>");
+		}
+	}
+}
+
+function MakeTOC(arrayTOC)
+{
+	if (arrayTOC.length > 0)
+	{
+		document.write("<ul id=\"TOCList\" class=\"TOCList\">");
+		document.write("<hr class=\"TOCLine\"><h4 class=\"TOCHeading\">&nbsp;&nbsp;ON THIS PAGE</h4><hr class=\"TOCLine\">");
+		BuildTOC(arrayTOC, g_nLevel);
+		document.write("<hr class=\"TOCLine\"></ul>");
+	}
+}
+
+function DoLogin(strTargetPassword)
+{
+	let inputPassword = document.getElementById("password");
+	let divContent = document.getElementById("course_content");
+	let divLogin = document.getElementById("login");
+	
+	if (inputPassword && divContent && divLogin)
+	{
+		//alert(window.atob(strTargetPassword) + ' ' + inputPassword.value);
+		if (inputPassword.value === window.atob(strTargetPassword))
+		{
+			divContent.style.display = "block";
+			divLogin.style.display = "none";
+		}
+	}
+}
+
+//**********************************************************************************************************************
+//**********************************************************************************************************************
+//** POPUP MENU FUNCTIONS
+//**********************************************************************************************************************
+//**********************************************************************************************************************
+
+function DoToggleCoursesPopupMenu()
+{
+	var strPopupName = "Courses";
+
+	if (document.getElementsByName(strPopupName) !== null)
+	{
+		if (g_bCoursesPopupMenu)
+		{
+			document.getElementsByName(strPopupName)[0].style.display = "none";
+		}
+		else
+		{
+			document.getElementsByName(strPopupName)[0].style.display = "block";
+		}
+		g_bCoursesPopupMenu = !g_bCoursesPopupMenu;
+	}
+	else
+	{
+		alert("No such object with name '" + strPopupName + "'!");
+	}
+}
---
+++ GregsSTEMTuition/_vti_cnf/Common.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|DESKTOP-KFJ8F7O\\boyle
+vti_modifiedby:SR|DESKTOP-KFJ8F7O\\boyle
+vti_timelastmodified:TW|06 Aug 2023 12:43:15 -0000
+vti_timecreated:TR|05 Aug 2023 15:05:01 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|master.dwt courses/c_course.html courses/python_course.html pedogogy/pedogogy.html index.html Testimonials/testimonials.html courses/web_course.html courses/web_password.html
+vti_nexttolasttimemodified:TW|06 Aug 2023 12:43:15 -0000
+vti_cacheddtm:TX|06 Aug 2023 12:43:15 -0000
+vti_filesize:IR|4124
