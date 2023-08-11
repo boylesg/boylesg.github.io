cc4f64c15e5ea5912362d72d5b47420b124f80f4
Gregary Boyles <gregplants@bigpond.com>
2023-08-11 17:45:11 +1000

    Add files via upload

--- GregsSTEMTuition/Common.js
+++ GregsSTEMTuition/Common.js
@@ -83,44 +83,6 @@ function MakeTOC(arrayTOC)
 	}
 }
 
-function DoLogin(strTargetPassword, strCourseName)
-{
-	let inputPassword = document.getElementById("password");
-	let divContent = document.getElementById("course_content");
-	let divLogin = document.getElementById("login");
-
-	if (inputPassword && divContent && divLogin)
-	{
-		if ((inputPassword.value === strTargetPassword) || ((sessionStorage[strCourseName]) && (sessionStorage[strCourseName].length > 0)))
-		{
-			divContent.style.display = "block";
-			divLogin.style.display = "none";
-			sessionStorage[strCourseName] = strTargetPassword;
-			if (sessionStorage["current_stage"] && (sessionStorage["current_stage"].length > 0))
-				document.getElementById(sessionStorage["current_stage"]).style.display = "block";
-			else
-				document.getElementById("Stage1").style.display = "block";
-		}
-	}
-}
-
-function DoShowHide(strIDDiv2Hide, strIDDiv2Show)
-{
-	var div2Hide = document.getElementById(strIDDiv2Hide),
-		div2Show = document.getElementById(strIDDiv2Show);
-		
-	if (div2Hide)
-	{
-		div2Hide.style.display = "none";
-	}
-	if (div2Show)
-	{
-		div2Show.style.display = "block";
-		sessionStorage["current_stage"] = strIDDiv2Show;
-		//alert(sessionStorage["current_stage"]);
-	}
-}
-
 //**********************************************************************************************************************
 //**********************************************************************************************************************
 //** POPUP MENU FUNCTIONS
@@ -148,3 +110,99 @@ function DoToggleCoursesPopupMenu()
 		alert("No such object with name '" + strPopupName + "'!");
 	}
 }
+
+//**********************************************************************************************************************
+//**********************************************************************************************************************
+//** COURSE FUNCTIONS
+//**********************************************************************************************************************
+//**********************************************************************************************************************
+
+var g_nStageNum = 1;
+
+function DoLogin(strTargetPassword, strCourseName)
+{
+	let inputPassword = document.getElementById("password");
+	let divContent = document.getElementById("course_content");
+	let divLogin = document.getElementById("login");
+
+	if (inputPassword && divContent && divLogin)
+	{
+		if ((inputPassword.value === strTargetPassword) || ((sessionStorage[strCourseName]) && (sessionStorage[strCourseName].length > 0)))
+		{
+			divContent.style.display = "block";
+			divLogin.style.display = "none";
+			sessionStorage[strCourseName] = strTargetPassword;
+			if (sessionStorage["current_stage"] && (sessionStorage["current_stage"].length > 0))
+				document.getElementById(sessionStorage["current_stage"]).style.display = "block";
+			else
+				document.getElementById("Stage1").style.display = "block";
+		}
+	}
+}
+
+function DoShowHide(strIDDiv2Hide, strIDDiv2Show)
+{
+	var div2Hide = document.getElementById(strIDDiv2Hide),
+		div2Show = document.getElementById(strIDDiv2Show);
+
+	if (div2Hide)
+	{
+		div2Hide.style.display = "none";
+	}
+	if (div2Show)
+	{
+		div2Show.style.display = "block";
+		sessionStorage["current_stage"] = strIDDiv2Show;
+		//alert(sessionStorage["current_stage"]);
+	}
+}
+
+function DrawFirstStageButtons(strStartPage)
+{
+	let nNextStageNum =  g_nStageNum + 1;
+	
+	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"document.location='" + strStartPage + "'\">&lt; PREVIOUS</button>&nbsp;");
+	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage" + nNextStageNum.toString() + "', 'Stage" + nNextStageNum + "')\">NEXT &gt;</button>");
+	g_nStageNum++;
+}
+
+function DrawLastStageButtons(strStartPage)
+{
+	let nPreviousStageNum =  g_nStageNum - 1;
+	
+	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage" + nPreviousStageNum.toString() + "', 'Stage" + nPreviousStageNum + "')\">&lt; PREVIOUS</button>");
+	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"document.location='" + strStartPage + "'\">NEXT &gt;</button>&nbsp;");
+	g_nStageNum++;
+}
+
+function DrawMidStageButtons()
+{
+	let nCurrentStageNum = g_nStageNum,
+		nNextStageNum =  g_nStageNum + 1,
+		nPreviousStageNum = g_nStageNum - 1;
+
+	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage" + nCurrentStageNum.toString() + "', 'Stage" + nPreviousStageNum.toString() + "')\">&lt; PREVIOUS</button>&nbsp;");
+	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage" + nCurrentStageNum.toString() + "', 'Stage" + nNextStageNum.toString() + "')\">NEXT &gt;</button>");
+	g_nStageNum++;
+}
+
+function SetStageDivIDs()
+{
+	let divStage = null;
+	let nI = 1;
+
+	do
+	{
+		divStage = document.getElementById("Stage");
+
+		if (divStage)
+		{
+			divStage.id = "Stage" + nI.toString();
+			nI++;
+		}
+	}
+	while (divStage);
+}
+
+
+
