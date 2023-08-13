7570789bb13d2fee0b46692c22996245c207cf5d
Gregary Boyles <gregplants@bigpond.com>
2023-08-14 00:56:47 +1000

    Add files via upload

--- GregsSTEMTuition/Common.js
+++ GregsSTEMTuition/Common.js
@@ -6,6 +6,7 @@
 
 var g_arrayTOC = [];
 var g_bCoursesPopupMenu = false;
+var g_nStageNum = 1;
 
 
 
@@ -117,14 +118,44 @@ function DoToggleCoursesPopupMenu()
 //**********************************************************************************************************************
 //**********************************************************************************************************************
 
-var g_nStageNum = 1;
+var g_arrayStageBookmarks = [];
+
+function DoHighlightCurrentStageLink(strIDLink2Highlight, strIDLink2Unhighlight)
+{
+	let headingStage2Highlight = document.getElementById(strIDLink2Highlight);
+	let headingStage2Unhighlight = document.getElementById(strIDLink2Unhighlight);
+	
+	if (headingStage2Highlight)
+	{
+		let strIDLink2Highlight = headingStage2Highlight.innerText;
+		let link2Highlight = document.getElementById(strIDLink2Highlight);
+		
+		if (link2Highlight)
+		{
+			link2Highlight.style.backgroundColor = "#DDCC99";
+			link2Highlight.style.color = "red";
+		}
+	}
+	if (headingStage2Unhighlight)
+	{
+		let strIDLink2Unhighlight = headingStage2Unhighlight.innerText;
+		let link2Unhighlight = document.getElementById(strIDLink2Unhighlight);
+		
+		if (link2Unhighlight)
+		{
+			link2Unhighlight.style.backgroundColor = "#CCBB88";
+			link2Unhighlight.style.color = "navy";
+		}
+	}
+}
 
 function DoLogin(strTargetPassword, strCourseName)
 {
 	let inputPassword = document.getElementById("password");
 	let divContent = document.getElementById("course_content");
 	let divLogin = document.getElementById("login");
-
+	let divContentHeader = document.getElementById("ContentHeader");
+	
 	if (inputPassword && divContent && divLogin)
 	{
 		if ((inputPassword.value === strTargetPassword) || ((sessionStorage[strCourseName]) && (sessionStorage[strCourseName].length > 0)))
@@ -133,76 +164,158 @@ function DoLogin(strTargetPassword, strCourseName)
 			divLogin.style.display = "none";
 			sessionStorage[strCourseName] = strTargetPassword;
 			if (sessionStorage["current_stage"] && (sessionStorage["current_stage"].length > 0))
+			{
+				//console.log("sessionStorage['current_stage'] = " + sessionStorage["current_stage"]);
 				document.getElementById(sessionStorage["current_stage"]).style.display = "block";
-			else
+			}
+			else if (document.getElementById('Stage1'))
+			{
+				//console.log("document.getElementById('Stage1') = " + document.getElementById("Stage1"));
 				document.getElementById("Stage1").style.display = "block";
+			}
+			divContentHeader.style.display = "block";
+			
+			if (!sessionStorage["current_stage"] || (sessionStorage["current_stage"].length == 0))
+				sessionStorage["current_stage"] = "Stage1";
+								
+			DoHighlightCurrentStageLink(sessionStorage["current_stage"] + "Heading", "");
 		}
 	}
 }
 
-function DoShowHide(strIDDiv2Hide, strIDDiv2Show)
+function DoShowHide(strIDDiv2Show, strIDDiv2Hide)
 {
 	var div2Hide = document.getElementById(strIDDiv2Hide),
-		div2Show = document.getElementById(strIDDiv2Show);
+		div2Show = document.getElementById(strIDDiv2Show),
+		strIDLink2Highlight = "", 
+		strIDLink2Unhighlight = "";
 
 	if (div2Hide)
 	{
 		div2Hide.style.display = "none";
+		strIDLink2Unhighlight = strIDDiv2Hide + "Heading";
+	}
+	else
+	{
+		strIDLink2Unhighlight = "Stage1Heading";
 	}
 	if (div2Show)
 	{
 		div2Show.style.display = "block";
+		sessionStorage["previous_stage"] = sessionStorage["current_stage"];
 		sessionStorage["current_stage"] = strIDDiv2Show;
+		strIDLink2Highlight = strIDDiv2Show + "Heading";
 		//alert(sessionStorage["current_stage"]);
 	}
+	DoHighlightCurrentStageLink(strIDLink2Highlight, strIDLink2Unhighlight);
 }
 
-function DrawFirstStageButtons(strStartPage)
+function DrawFirstStageButtons(strStartPage, nStageNum)
 {
-	let nNextStageNum =  g_nStageNum + 1;
-	
+	g_nStageNum++;
 	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"document.location='" + strStartPage + "'\">&lt; PREVIOUS</button>&nbsp;");
-	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage" + nNextStageNum.toString() + "', 'Stage" + nNextStageNum + "')\">NEXT &gt;</button>");
-	g_nStageNum++;
-}
-
-function DrawLastStageButtons(strStartPage)
-{
-	let nPreviousStageNum =  g_nStageNum - 1;
+	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage2', 'Stage1')\">NEXT &gt;</button>");
 	
-	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage" + nPreviousStageNum.toString() + "', 'Stage" + nPreviousStageNum + "')\">&lt; PREVIOUS</button>");
-	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"document.location='" + strStartPage + "'\">NEXT &gt;</button>&nbsp;");
-	g_nStageNum++;
+	return nStageNum + 1;
 }
 
-function DrawMidStageButtons()
+function DrawLastStageButtons(nStageNum)
+{	
+	nStageNum--;
+	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage" + nStageNum.toString() + "', 'Stage" + nPreviousStageNum + "')\">&lt; PREVIOUS</button>");
+	
+	return nStageNum + 1;
+}
+
+function DrawMidStageButtons(nStageNum)
 {
-	let nCurrentStageNum = g_nStageNum,
-		nNextStageNum =  g_nStageNum + 1,
-		nPreviousStageNum = g_nStageNum - 1;
+	let nNextStageNum = nStageNum + 1;
+	let nPrevStageNum = nStageNum - 1;
 
-	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage" + nCurrentStageNum.toString() + "', 'Stage" + nPreviousStageNum.toString() + "')\">&lt; PREVIOUS</button>&nbsp;");
-	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage" + nCurrentStageNum.toString() + "', 'Stage" + nNextStageNum.toString() + "')\">NEXT &gt;</button>");
-	g_nStageNum++;
+	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage" + nPrevStageNum.toString() + "', 'Stage" + nStageNum.toString() + "')\">&lt; PREVIOUS</button>&nbsp;");
+	document.write("<button type=\"button\" class=\"PreviousNextButtons\" onclick=\"DoShowHide('Stage" + nNextStageNum.toString() + "', 'Stage" + nStageNum.toString() + "')\">NEXT &gt;</button>");
+	
+	return nStageNum + 1;
 }
 
-function SetStageDivIDs()
+function OnClickStageLink(strIDStageDiv2Show)
+{
+	DoShowHide(strIDStageDiv2Show, sessionStorage["current_stage"]);
+}
+
+function SetStageDivIDs(strStageLinkID)
 {
 	let divStage = null;
+	let headingStage = null;
+	let divContentHeader = document.getElementById("ContentHeader");
+	let divCourseContent = document.getElementById("course_content");
+	let divCode = null;
 	let nI = 1;
 
 	do
 	{
 		divStage = document.getElementById("Stage");
+		headingStage = document.getElementById("StageHeading");
 
 		if (divStage)
 		{
 			divStage.id = "Stage" + nI.toString();
+			if (headingStage)
+			{
+				headingStage.id = "Stage" + nI.toString() + "Heading";
+				g_arrayStageBookmarks.push("<a href=\"#\" class=\"StageLink\" id=\"" + 
+											headingStage.innerText + "\" onclick=\"OnClickStageLink('" +
+											divStage.id + "') \">" + headingStage.innerText + "</a>");
+			}
 			nI++;
 		}
 	}
 	while (divStage);
+	
+	if (divContentHeader && divCourseContent)
+	{
+		console.log(g_arrayStageBookmarks);
+		divContentHeader.style.display = divCourseContent.style.display;
+		for (let nI = 0; nI < g_arrayStageBookmarks.length; nI++)
+		{
+			divContentHeader.innerHTML += g_arrayStageBookmarks[nI];
+		}
+	}
+
 }
 
+function WriteAsHTMLTags(arrayLinesHTML)
+{
+	for (let nI = 0; nI < arrayLinesHTML.length; nI++)
+	{
+		document.write(arrayLinesHTML[nI]);
+	}
+}
+
+function Replace(strText, strReplaceWhat, strReplaceWith)
+{
+	let nI = strText.indexOf(strReplaceWhat);
+	
+	while (nI > -1)
+	{
+		strText = strText.replace(strReplaceWhat, strReplaceWith);
+		nI = strText.indexOf(strReplaceWhat);
+	}
+	return strText;
+}
+
+function WriteAsHTMLCode(arrayLinesHTML)
+{
+	for (let nI = 0; nI < arrayLinesHTML.length; nI++)
+	{
+		let strLineHTML = arrayLinesHTML[nI];
+		strLineHTML = Replace(strLineHTML, " ", "&nbsp;&nbsp;");
+		strLineHTML = Replace(strLineHTML, "<", "&lt;");
+		strLineHTML = Replace(strLineHTML, ">", "&gt;");
+		if (strLineHTML.indexOf("</script_>"))
+			strLineHTML = strLineHTML.replace("</script_>", "</script>")
+		document.write(strLineHTML + "<br/>");
+	}
+}
 
 
