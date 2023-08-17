0cefe5d82d6f442245fcf32fca687bf0a7f3d2aa
Gregary Boyles <gregplants@bigpond.com>
2023-08-18 01:23:58 +1000

    Add files via upload

--- GregsSTEMTuition/_vti_cnf/Common.js
+++ GregsSTEMTuition/_vti_cnf/Common.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|16 Aug 2023 16:56:37 -0000
+vti_timelastmodified:TR|17 Aug 2023 12:59:20 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|DESKTOP-KFJ8F7O\\boyle
 vti_modifiedby:SR|DESKTOP-KFJ8F7O\\boyle
 vti_timecreated:TR|16 Aug 2023 06:18:44 -0000
 vti_backlinkinfo:VX|courses/courses.dwt master.dwt education/education.html courses/c_course.html courses/web_course_css.html editing.html work/work.html pedogogy/pedogogy.html courses/python_course.html index.html testimonials/testimonials.html courses/web_password.html courses/web_course.html teaching/teaching.html courses/web_course_html.html
-vti_nexttolasttimemodified:TW|16 Aug 2023 16:56:25 -0000
-vti_cacheddtm:TX|16 Aug 2023 16:56:37 -0000
-vti_filesize:IR|38238
+vti_nexttolasttimemodified:TW|16 Aug 2023 16:56:37 -0000
+vti_cacheddtm:TX|17 Aug 2023 12:59:20 -0000
+vti_filesize:IR|38274
