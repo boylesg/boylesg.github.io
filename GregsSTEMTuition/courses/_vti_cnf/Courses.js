dc74c4b61109848fcc03884b27e4b5abf7a57d73
Gregary Boyles <gregplants@bigpond.com>
2023-10-04 00:33:20 +1100

    Add files via upload

--- GregsSTEMTuition/courses/_vti_cnf/Courses.js
+++ GregsSTEMTuition/courses/_vti_cnf/Courses.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|DESKTOP-KFJ8F7O\\boyle
 vti_modifiedby:SR|DESKTOP-KFJ8F7O\\boyle
-vti_timelastmodified:TR|23 Aug 2023 09:10:26 -0000
+vti_timelastmodified:TR|03 Oct 2023 06:29:36 -0000
 vti_timecreated:TR|18 Aug 2023 13:08:52 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|courses/courses.dwt courses/c_course.html courses/web_course_css.html courses/HexNumbers.html courses/python_course.html courses/editing.html courses/colors.html courses/web_course.html courses/web_course_html.html
-vti_nexttolasttimemodified:TW|23 Aug 2023 09:06:45 -0000
-vti_cacheddtm:TX|23 Aug 2023 09:10:26 -0000
-vti_filesize:IR|21784
+vti_backlinkinfo:VX|courses/courses.dwt courses/BinaryNumbers.html courses/c_course.html courses/web_course_css.html courses/HexNumbers.html courses/web_course_javascript.html courses/python_course.html courses/editing.html courses/colors.html courses/web_course.html courses/web_course_html.html courses/javascript_event_names.html courses/web_course_page.html
+vti_nexttolasttimemodified:TW|03 Oct 2023 06:27:03 -0000
+vti_cacheddtm:TX|03 Oct 2023 06:29:36 -0000
+vti_filesize:IR|82266
