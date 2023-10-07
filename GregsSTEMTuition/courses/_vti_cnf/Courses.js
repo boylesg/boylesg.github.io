14cbeaa555e597b61bddf4dab9d2fcdd9ca108a4
boylesg <gregplants@bigpond.com>
2023-10-08 00:48:57 +1100

    Update

--- GregsSTEMTuition/courses/_vti_cnf/Courses.js
+++ GregsSTEMTuition/courses/_vti_cnf/Courses.js
@@ -4,7 +4,7 @@ vti_modifiedby:SR|DESKTOP-KFJ8F7O\\boyle
 vti_timelastmodified:TR|03 Oct 2023 23:10:22 -0000
 vti_timecreated:TR|18 Aug 2023 13:08:52 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|courses/courses.dwt courses/BinaryNumbers.html courses/c_course.html courses/web_course_css.html courses/HexNumbers.html courses/web_course_javascript.html courses/python_course.html courses/editing.html courses/colors.html courses/web_course.html courses/web_course_html.html courses/javascript_event_names.html courses/web_course_page.html
+vti_backlinkinfo:VX|courses/courses.dwt courses/BinaryNumbers.html courses/c_course.html courses/web_course_css.html courses/HexNumbers.html courses/web_course_javascript.html courses/python_course.html courses/editing.html courses/colors.html courses/web_course.html courses/web_course_html.html courses/javascript_event_names.html courses/web_course_page.html courses/web_course_build.html
 vti_nexttolasttimemodified:TW|03 Oct 2023 23:06:30 -0000
 vti_cacheddtm:TX|03 Oct 2023 23:10:22 -0000
 vti_filesize:IR|82463
