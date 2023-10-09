f6f28369d547aa0363a58db7c4c6282af7515bd6
boylesg <gregplants@bigpond.com>
2023-10-09 22:59:54 +1100

    Update

--- GregsSTEMTuition/_vti_cnf/Common.js
+++ GregsSTEMTuition/_vti_cnf/Common.js
@@ -4,7 +4,7 @@ vti_extenderversion:SR|12.0.0.0
 vti_author:SR|DESKTOP-KFJ8F7O\\boyle
 vti_modifiedby:SR|DESKTOP-KFJ8F7O\\boyle
 vti_timecreated:TR|16 Aug 2023 06:18:44 -0000
-vti_backlinkinfo:VX|courses/courses.dwt master.dwt education/education.html courses/c_course.html courses/web_course_css.html courses/HexNumbers.html courses/web_course_javascript.html courses/python_course.html work/work.html pedogogy/pedogogy.html courses/colors.html index.html testimonials/testimonials.html courses/web_password.html courses/web_course.html teaching/teaching.html courses/web_course_html.html courses/javascript_event_names.html courses/web_course_page.html courses/course.dwt courses/web_course_build.html courses/BinaryNumbers.html
+vti_backlinkinfo:VX|courses/courses.dwt master.dwt education/education.html courses/c_course.html courses/web_course_css.html courses/HexNumbers.html courses/web_course_javascript.html courses/python_course.html work/work.html pedogogy/pedogogy.html courses/colors.html index.html testimonials/testimonials.html courses/web_password.html courses/web_course.html teaching/teaching.html courses/web_course_html.html courses/javascript_event_names.html courses/web_course_page.html courses/web_course_build.html courses/BinaryNumbers.html
 vti_nexttolasttimemodified:TW|02 Oct 2023 07:57:05 -0000
 vti_cacheddtm:TX|02 Oct 2023 07:57:06 -0000
 vti_filesize:IR|17988
---
+++ GregsSTEMTuition/courses/code/login/common.js
@@ -0,0 +1,86 @@
+﻿function CreateNavigationStructure()
+{	
+	document.write("<nav id=\"nav\" class=\"nav\">");
+	document.write("<a class=\"nav_link\" href=\"index.html\">Home</a>");
+	document.write("<br/>");
+	document.write("<a class=\"nav_link\" onclick=\"DoShowHidePopupMenu('products_popup')\">Products</a>");
+	document.write("<div id=\"products_popup\" class=\"popup\" style=\"display:none\">");
+	document.write("<a class=\"popup_link\" href=\"televisions.html\">Televisions</a><br/>");
+	document.write("<a class=\"popup_link\" href=\"DVDplayers.html\">DVD Players</a><br/>");
+	document.write("<a class=\"popup_link\" href=\"surround.html\">Surround sound</a><br/>");
+	document.write("<a class=\"popup_link\" href=\"games.html\">Game consoles</a>");
+	document.write("</div><br/>");
+	document.write("<a class=\"nav_link\" href=\"services.html\">Services</a>");
+	document.write("<br/>");
+	document.write("<a class=\"nav_link\" href=\"contact.html\">Contact</a>");
+	document.write("</nav>");
+}
+
+function DoShowHidePopupMenu(strID)
+{
+	let divPopup = document.getElementById(strID);
+	
+	// Make sure we have a valid ID and that we have a valid popup object
+	if (divPopup)
+	{
+		// If the popup is visible
+		if (divPopup.style.display == "block")
+			// The hide it
+			divPopup.style.display = "none";
+		// Else if the popup is hidden
+		else if (divPopup.style.display == "none")
+			// The show it
+			divPopup.style.display = "block";
+	}
+}
+
+function DoOpenFooter()
+{
+	let divFooter = document.getElementById("footer"),
+		divActivator = document.getElementById("activator");
+	
+	if (divFooter && divActivator)
+	{
+		if (divFooter.clientHeight == 400)
+		{
+			divFooter.style.height = "80px";
+			divFooter.style.top = "0px";
+			divActivator.innerText = "▲";
+		}
+		else
+		{
+			divFooter.style.height = "400px";
+			divFooter.style.top = "-320px";
+			divActivator.innerText = "▼";
+		}
+	}
+}
+
+function GenerateFooterContent()
+{
+	document.write("Great company with great products - Fred Smith<br/><br/>");
+	document.write("Terrific customer service - Joe Bloggs<br/><br/>");
+}
+
+function DoLogin()
+{
+	let inputPassword = document.getElementById("text_password"),
+		divLogin = document.getElementById("login"),
+		divContent = document.getElementById("page_content");
+	
+	// Check if the id were valid and that we have a valid objects
+	if (inputPassword && divLogin && divContent)
+	{
+		// If the password is correct - all text and password input objects 
+		// have the property 'value' which contains the text typed by the 
+		// user
+		if (inputPassword.value == "password")
+		{
+			// Hide the div containing the login form
+			divLogin.style.display = "none";// The javascript 'display' property corresponds to the CSS attribute 'display' 
+			// Show the div containing the page contents
+			divContent.style.display = "inline-block";
+		}
+	}
+}
+
