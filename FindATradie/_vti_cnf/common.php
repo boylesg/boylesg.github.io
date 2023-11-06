da3dfbb932805d88455b8b6473737b17bdbbcc24
boylesg <gregplants@bigpond.com>
2023-11-07 01:54:47 +1100

    Update

--- FindATradie/_vti_cnf/common.php
+++ FindATradie/_vti_cnf/common.php
@@ -1,13 +1,13 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|04 Nov 2023 12:24:48 -0000
+vti_timelastmodified:TR|06 Nov 2023 14:16:36 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|DESKTOP-KFJ8F7O\\boyle
 vti_modifiedby:SR|DESKTOP-KFJ8F7O\\boyle
 vti_timecreated:TR|26 Oct 2023 15:38:31 -0000
 vti_backlinkinfo:VX|
 vti_nexttolasttimemodified:TW|04 Nov 2023 12:24:48 -0000
-vti_cacheddtm:TX|04 Nov 2023 11:56:20 -0000
-vti_filesize:IR|7661
+vti_cacheddtm:TX|06 Nov 2023 14:16:36 -0000
+vti_filesize:IR|7763
 vti_cachedneedsrewrite:BR|false
 vti_cachedhasbots:BR|false
 vti_cachedhastheme:BR|false
--- FindATradie/common.php
+++ FindATradie/common.php
@@ -1,6 +1,9 @@
 <?php
-
-	session_save_path("c:/users/boyle/temp");
+	
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);	
+	//session_save_path("c:/users/boyle/temp");
 	session_start();
 	
 	//******************************************************************************
