20e50461d6ec53884934460e7975e4ad8b6b433f
Gregary Boyles <gregplants@bigpond.com>
2024-02-24 14:38:41 +1100

    Update

---
+++ FindATradie/app/_vti_cnf/pay_advert.php
@@ -0,0 +1,21 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|GREGS-LAPTOP\\boyle
+vti_modifiedby:SR|LATITUDE-E6410\\gregaryb
+vti_timelastmodified:TR|24 Feb 2024 03:36:45 -0000
+vti_timecreated:TR|18 Feb 2024 05:31:30 -0000
+vti_title:SR|RENEW MEMBERSHIP
+vti_extenderversion:SR|12.0.0.0
+vti_nexttolasttimemodified:TW|24 Feb 2024 03:20:56 -0000
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|24 Feb 2024 03:36:45 -0000
+vti_filesize:IR|5926
+vti_cachedtitle:SR|RENEW MEMBERSHIP
+vti_cachedbodystyle:SR|<body>
+vti_cachedlinkinfo:VX|Q|styles/paypal_style.css A|https://www.paypal.com/cgi-bin/webscr S|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif A|https://www.paypal.com/cgi-bin/webscr S|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif A|https://www.paypal.com/cgi-bin/webscr S|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif A|https://www.sandbox.paypal.com/cgi-bin/webscr S|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif A|https://www.sandbox.paypal.com/cgi-bin/webscr S|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif A|https://www.sandbox.paypal.com/cgi-bin/webscr S|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif
+vti_cachedsvcrellinks:VX|NQUS|app/styles/paypal_style.css NASS|https://www.paypal.com/cgi-bin/webscr NSSS|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif NASS|https://www.paypal.com/cgi-bin/webscr NSSS|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif NASS|https://www.paypal.com/cgi-bin/webscr NSSS|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif NASS|https://www.sandbox.paypal.com/cgi-bin/webscr NSSS|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif NASS|https://www.sandbox.paypal.com/cgi-bin/webscr NSSS|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif NASS|https://www.sandbox.paypal.com/cgi-bin/webscr NSSS|https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8
+vti_charset:SR|utf-8
---
+++ FindATradie/app/pay_advert.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	
+<html xmlns="http://www.w3.org/1999/xhtml">
+	
+	<head>
+		<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+		<title>RENEW MEMBERSHIP</title>
+		<link href="styles/paypal_style.css" media="screen" rel="stylesheet" title="CSS" type="text/css" />
+		<?php 
+		
+			require_once "../common.php";
+			
+			$g_strDisplay10 = "";
+			$g_strDisplay80 = "";
+			$g_strDisplay100 = "";
+			
+			if (isset($_GET["paypal"]))
+			{
+				if (strcmp($_GET["paypal"], "10") == 0)
+				{
+					$g_strDisplay10 = "block";
+					$g_strDisplay80 = "none";
+					$g_strDisplay100 = "none";
+				}
+				else if (strcmp($_GET["paypal"], "80") == 0)
+				{
+					$g_strDisplay10 = "none";
+					$g_strDisplay80 = "block";
+					$g_strDisplay100 = "none";
+				}
+				else if (strcmp($_GET["paypal"], "100") == 0)
+				{
+					$g_strDisplay10 = "none";
+					$g_strDisplay80 = "none";
+					$g_strDisplay100 = "block";
+				}
+			}
+		
+		?>
+	</head>
+	
+
+	<body>
+	
+		<div id="paypal_live" style="display:<?php echo $g_strPaypalLive; ?>">
+			<table cellpadding="7" cellspacing="0" border="1" style="display:<?php echo $g_strDisplay10; ?>">
+				<tr>
+					<td class="table_cell">
+						$10
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+							<input type="hidden" name="cmd" value="_s-xclick" />
+							<input type="hidden" name="hosted_button_id" value="VF7D5V7W4CPJ4" />
+							<input type="hidden" name="currency_code" value="AUD" />
+							<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+						</form>
+					</td>
+				</tr>
+			</table>
+			<table cellpadding="7" cellspacing="0" border="1" style="display:<?php echo $g_strDisplay80; ?>">
+				<tr>
+					<td class="table_cell">
+						$80
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+							<input type="hidden" name="cmd" value="_s-xclick" />
+							<input type="hidden" name="hosted_button_id" value="F4FH6AVBNP2YS" />
+							<input type="hidden" name="currency_code" value="AUD" />
+							<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+						</form>
+					</td>
+				</tr>
+			</table>
+			<table cellpadding="7" cellspacing="0" border="1" style="display:<?php echo $g_strDisplay100; ?>">
+				<tr>
+					<td class="table_cell">
+						$100
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+							<input type="hidden" name="cmd" value="_s-xclick" />
+							<input type="hidden" name="hosted_button_id" value="78WNB6PA7CP4A" />
+							<input type="hidden" name="currency_code" value="AUD" />
+							<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+						</form>
+					</td>
+				</tr>
+			</table>
+		</div>
+		
+		<div id="paypal_test" style="display:<?php echo $g_strPaypalTest; ?>">
+		
+			<table cellpadding="7" cellspacing="0" border="1" style="display:<?php echo $g_strDisplay10; ?>">
+				<!--10-->
+				<tr>
+					<td class="table_cell">
+						$10
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+							<input type="hidden" name="cmd" value="_s-xclick" />
+							<input type="hidden" name="hosted_button_id" value="RJ5N5VXSSTGBG" />
+							<input type="hidden" name="currency_code" value="AUD" />
+							<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+						</form>
+					</td>
+				</tr>
+			</table>
+			<table cellpadding="7" cellspacing="0" border="1" style="display:<?php echo $g_strDisplay80; ?>">
+				<tr>
+					<td class="table_cell">
+						$80
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+							<input type="hidden" name="cmd" value="_s-xclick" />
+							<input type="hidden" name="hosted_button_id" value="DJKLEWWU3SHHA" />
+							<input type="hidden" name="currency_code" value="AUD" />
+							<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+						</form>
+					</td>
+				</tr>
+			</table>
+			<table cellpadding="7" cellspacing="0" border="1" style="display:<?php echo $g_strDisplay100; ?>">
+				<tr>
+					<td class="table_cell">
+						$100
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+							<input type="hidden" name="cmd" value="_s-xclick" />
+							<input type="hidden" name="hosted_button_id" value="W22A5JKGRMJXY" />
+							<input type="hidden" name="currency_code" value="AUD" />
+							<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Buy Now" />
+						</form>				
+					</td>
+				</tr>
+			</table>
+			
+		</div>
+
+	</body>
+	
+	<?php
+		
+		if (isset($_GET["paypal"]))
+		{
+			PrintJavascriptLine("AppInventor.setWebViewString(paypal_membership_payment=" . $_GET["paypal"] . ");", 2, true);
+		}
+	
+	?>
+	
+</html>
