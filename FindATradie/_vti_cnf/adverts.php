567806cb4db2c7a07d74de16f23af4b0f6e41803
Gregary Boyles <gregplants@bigpond.com>
2024-02-25 14:19:52 +1100

    Update

---
+++ FindATradie/_vti_cnf/adverts.php
@@ -0,0 +1,15 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|LATITUDE-E6410\\gregaryb
+vti_modifiedby:SR|LATITUDE-E6410\\gregaryb
+vti_timelastmodified:TR|25 Feb 2024 03:19:41 -0000
+vti_timecreated:TR|25 Feb 2024 03:18:39 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|25 Feb 2024 03:19:29 -0000
+vti_cacheddtm:TX|25 Feb 2024 03:19:41 -0000
+vti_filesize:IR|306
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
---
+++ FindATradie/adverts.php
@@ -0,0 +1,25 @@
+<?php
+
+	$g_bIsApp = true;
+	require_once "../common.php";
+
+	if (isset($_POST["button"]))
+	{
+		if ($_POST["button"] == "")
+		{
+		}
+		else if ($_POST["button"] == "")
+		{
+		}
+		else if ($_POST["button"] == "")
+		{
+		}
+		else if ($_POST["button"] == "")
+		{
+		}
+		else
+		{
+		}
+	}
+	
+?>
