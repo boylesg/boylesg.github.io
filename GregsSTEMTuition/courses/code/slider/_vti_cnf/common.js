caaaff6ed017be5298be91963507ec9dd57aaa39
Gregary Boyles <gregplants@bigpond.com>
2025-01-14 23:01:17 +1100

    Delete GregsSTEMTuition directory

--- GregsSTEMTuition/courses/code/slider/_vti_cnf/common.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|08 Oct 2023 11:01:08 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_author:SR|DESKTOP-KFJ8F7O\\boyle
-vti_modifiedby:SR|DESKTOP-KFJ8F7O\\boyle
-vti_timecreated:TR|08 Oct 2023 11:01:08 -0000
-vti_cacheddtm:TX|08 Oct 2023 11:01:08 -0000
-vti_filesize:IR|2111
-vti_backlinkinfo:VX|courses/code/slider/products.html courses/code/slider/services.html courses/code/slider/surround.html courses/code/slider/televisions.html courses/code/slider/contact.html courses/code/slider/DVDplayers.html courses/code/slider/games.html courses/code/slider/index.html courses/web_course_page.html
--- GregsSTEMTuition/courses/code/televisions/_vti_cnf/common.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|10 Oct 2023 07:41:04 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_author:SR|DESKTOP-KFJ8F7O\\boyle
-vti_modifiedby:SR|DESKTOP-KFJ8F7O\\boyle
-vti_timecreated:TR|10 Oct 2023 07:41:04 -0000
-vti_cacheddtm:TX|10 Oct 2023 07:41:04 -0000
-vti_filesize:IR|3176
-vti_backlinkinfo:VX|courses/code/televisions/contact.html courses/code/televisions/DVDplayers.html courses/code/televisions/games.html courses/code/televisions/televisions.html courses/code/televisions/index.html courses/code/televisions/products.html courses/code/televisions/services.html courses/code/televisions/surround.html
