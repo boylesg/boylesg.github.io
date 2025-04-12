d3c7270c1043624b1bda242e75b753f594cf35ef
Gregary Boyles <gregplants@bigpond.com>
2025-04-13 01:45:46 +1000

    Update

--- GregsSTEMTuition/courses/code/navigation/_vti_cnf/common.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|06 Oct 2023 08:17:27 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|courses/code/navigation/contact.html courses/code/navigation/products.html courses/code/navigation/services.html courses/code/navigation/index.html courses/web_course_page.html
-vti_author:SR|DESKTOP-KFJ8F7O\\boyle
-vti_modifiedby:SR|DESKTOP-KFJ8F7O\\boyle
-vti_timecreated:TR|06 Oct 2023 08:17:27 -0000
-vti_cacheddtm:TX|06 Oct 2023 08:17:27 -0000
-vti_filesize:IR|516
--- GregsSTEMTuition/courses/code/popups/_vti_cnf/common.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|06 Oct 2023 08:17:27 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|courses/code/popups/services.html courses/code/popups/surround.html courses/code/popups/televisions.html courses/code/popups/games.html courses/code/popups/DVDplayers.html courses/code/popups/contact.html courses/code/popups/index.html courses/web_course_page.html
-vti_author:SR|DESKTOP-KFJ8F7O\\boyle
-vti_modifiedby:SR|DESKTOP-KFJ8F7O\\boyle
-vti_timecreated:TR|06 Oct 2023 13:08:01 -0000
-vti_cacheddtm:TX|06 Oct 2023 13:08:01 -0000
-vti_filesize:IR|1453
